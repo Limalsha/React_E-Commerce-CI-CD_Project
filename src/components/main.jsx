975072721ad4164f2f6755cb6f9c53022a84ffba
Limalsha <limalshau@gmail.com>
2025-09-18 19:30:49 +0530

    first commit

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,17 +7,15 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main.png.jpg"
+            src="./assets/main1.png"
             alt="Card"
             height={500}
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
-              <p className="card-text fs-5 d-none d-sm-block ">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              <h5 className="card-title fs-1 fw-bold text-dark">New Season Arrivals</h5>
+              <p className="card-title fs-1 text-dark fw-lighter">
+                Being brave isn't about being different — it's about being yourself.
               </p>
             </div>
           </div>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,19 +8,7 @@ const AboutPage = () => {
         <h1 className="text-center">About Us</h1>
         <hr />
         <p className="lead text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
-          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
-          totam vero quas provident ipsam, veritatis nostrum velit quos
-          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
-          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
-          porro tempore quis eaque labore voluptatibus ea necessitatibus
-          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
-          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
-          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
-          delectus expedita a alias nam recusandae illo debitis repellat libero,
-          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
-          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+          At Emma Fashions, we believe style is more than just clothing—it’s an expression of individuality. Since our beginning, we’ve been committed to bringing modern, elegant, and timeless fashion to every wardrobe. Our collections are carefully curated to blend classic sophistication with contemporary trends, ensuring that you always step out with confidence. Whether it’s chic casual wear, bold statement pieces, or refined evening attire, Emma Fashions is dedicated to helping you look and feel your best.
         </p>
 
         <h2 className="text-center py-4">Our Products</h2>
